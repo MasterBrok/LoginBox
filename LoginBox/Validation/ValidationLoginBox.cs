90010ff21b40b1461df2e1a5dd61a5f8408a3f93
MesterBrok <Brokdeveloper@gmail.com>
2023-06-24 22:53:54 +0330

    Add project files.

---
+++ LoginBox/Validation/ValidationLoginBox.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace LoginBox.Validation
+{
+    public class ValidationLoginBox : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if (value is string result && !string.IsNullOrEmpty(result))
+            {
+                return ValidationResult.ValidResult;
+            }
+            return new ValidationResult(false,"Error");
+        }
+    }
+}
